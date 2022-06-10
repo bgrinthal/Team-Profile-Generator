6bad78d8b3832ff991c7f8ff590624315c860ebc
Ben Grinthal <bgrinthal@gmail.com>
2022-06-10 17:16:16 -0400

    initial commit

---
+++ pseudo_index.js
@@ -0,0 +1,26 @@
+/**********************************************************
+  REMOVE ALL THE COMMENTS BEFORE SUBMITTING YOUR HOMEWORK
+***********************************************************/
+
+// Imports classes for Manager, Engineer, Intern
+// Imports external package inquirer
+// Imports path
+// Imports fs
+
+// Imports page template source file
+
+// Create variables for the folder and the html file name
+
+// Create an empty array to store the team member objects
+
+// Create an empty array to store employee IDs to be used to check for the dupliates
+
+// **********************
+// Main Pfocess
+// **********************
+
+// Create a starter function to start the program
+//  1. function to create manager
+//      - inquire for manager's name, id, email, and office number
+//      - create manager object by initializing it with the properties in answer object
+
---
+++ pseudo_reset.js
@@ -0,0 +1,8 @@
+/**********************************************************
+  REMOVE ALL THE COMMENTS BEFORE SUBMITTING YOUR HOMEWORK
+***********************************************************/
+
+// Removes the team.html file from the dist/ folder.
+// Used in `npm run reset` script
+
+
